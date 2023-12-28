cf788f066160d4518da01de91fc9793712157038
jatinscodingproject <152329190+jatinscodingproject@users.noreply.github.com>
2023-12-29 02:11:50 +0530

    # adding a cart view page

--- models/cart.js
+++ models/cart.js
@@ -51,4 +51,14 @@ module.exports = class Cart{
             })
         })
     }
+    static getProduct(cb){
+        fs.readFile(p,(err,fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if(err){
+                cb(null);
+            }else{
+                cb(cart)
+            }
+        })
+    }
 }
